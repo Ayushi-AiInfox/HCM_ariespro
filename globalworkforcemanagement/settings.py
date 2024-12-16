74eb3881987fb967f68cc858497ad857ac40c717
Ayushi-AiInfox <ayushiraj@aiinfox.com>
2024-12-16 14:41:59 +0530

    Settings changed

--- globalworkforcemanagement/settings.py
+++ globalworkforcemanagement/settings.py
@@ -114,7 +114,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'workforcemanagement',
         'USER': 'root',
-        'PASSWORD': 'root@123',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306'
         },
